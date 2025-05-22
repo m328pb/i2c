0658acee13dd7429457629ecf4b6126eff4e9144
zielaskowski <michal.zielaskowski@gmail.com>
2025-05-22 23:59:53 +0200

    ssd1306 separate from protocol

--- i2c.cpp
+++ i2c.cpp
@@ -1,17 +1,21 @@
+#pragma once
 #include "i2c.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stdint.h>
 
-void mw_init() {
+I2C::I2C(uint8_t addr) { this->addr = addr; }
+
+void I2C::init() {
   PRR0 &= ~(1 << PRTWI0); // turn on TWI
-  TWCR0 &= ~(1 << TWIE); // turn off interrupts
+  TWCR0 &= ~(1 << TWIE);  // turn off interrupts
 
   /* twi bit rate formula from atmega328p manual pg 180
   SCL Frequency = CPU Clock Frequency / (16 + (2 * TWBR))
   SCL frequency up to 400kHz on atmega328p
   note: TWBR should be 10 or higher for master mode*/
-  TWSR0 &= ~(1 << TWPS0);                // initialize twi prescaler and bit rate
-  TWSR0 &= ~(1 << TWPS1);                // set prescaler to 1
+  TWSR0 &= ~(1 << TWPS0); // initialize twi prescaler and bit rate
+  TWSR0 &= ~(1 << TWPS1); // set prescaler to 1
   TWBR0 = ((F_CPU / I2C_FREQ) - 16) / 2; // 100kHz
 
   // activate internal pullups for twi.
@@ -27,13 +31,14 @@ void mw_init() {
  * @details
  * This function sends data to the slave device over I2C.
  * It sends a start condition, the slave device address (with W bit set),
- * the data, and a stop condition. It returns I2C_status (TWSR codes).
+ * the data, and a stop condition. It returns error struct.
  */
 
-I2C_status mw_write(uint8_t *data, uint8_t len)
+I2C::error I2C::write(uint8_t *data, uint8_t len)
 // must be 8 bit data
 {
-  I2C_status status = TW_NO_INFO;
+  status = TW_NO_INFO;
+  error err = NO_ERR;
   // Master Transmitter Mode (atmega328p, pg 185)
   // send start condition
   TWCR0 = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
@@ -42,12 +47,12 @@ I2C_status mw_write(uint8_t *data, uint8_t len)
   status = TWSR_code;
   if (status != TW_START) // A START condition has been transmitted
   {
-    mw_stop();
-    return status;
+    stop();
+    return  DEV_ERR;
   };
 
   // build sla+w, slave device address + w bit (0 write to slave)
-  TWDR0 = SSD1306_I2C_ADDRESS << 1;
+  TWDR0 = addr << 1;
   // transmit address
   TWCR0 = (1 << TWINT) | (1 << TWEN);
   while (!(TWCR0 & (1 << TWINT)))
@@ -55,8 +60,8 @@ I2C_status mw_write(uint8_t *data, uint8_t len)
   status = TWSR_code;
   if (status != TW_ACK) // SLA+W transmitted, ACK received
   {
-    mw_stop();
-    return status;
+    stop();
+    return DEV_ERR;
   };
 
   // send data
@@ -68,13 +73,13 @@ I2C_status mw_write(uint8_t *data, uint8_t len)
     status = TWSR_code;
     if (status != TW_DATA_ACK) // data byte transmitted, ACK received
     {
-      mw_stop();
-      return status;
+      stop();
+      return COM_ERR;
     };
   };
   // send stop condition
-  mw_stop();
-  return status;
+	stop();
+  return NO_ERR;
 }
 
-void mw_stop() { TWCR0 = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN); }
+void I2C::stop() { TWCR0 = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN); }
--- i2c.h
+++ i2c.h
@@ -6,10 +6,15 @@
 #define TWSR_mask 0xF8 // mask for TWSR register (7..3 bits)
 #define TWSR_code (I2C_status)(TWSR0 & TWSR_mask)
 
-// I2C protocol status codes (basically TWSR register bits)
-// see atmega328p datasheet, page 186, tab.21-3
-typedef enum : uint8_t
-{
+class I2C {
+private:
+  uint8_t addr;
+
+public:
+  I2C(uint8_t addr);
+  // I2C protocol status codes (basically TWSR register bits)
+  // see atmega328p datasheet, page 186, tab.21-3
+  typedef enum : uint8_t {
     TW_NO_INFO = 0x00,
     TW_START = 0x08,
     TW_REP_START = 0x10,
@@ -18,8 +23,16 @@ typedef enum : uint8_t
     TW_DATA_ACK = 0x28,
     TW_DATA_NACK = 0x30,
     TW_ARBIT_LOST = 0x38
-} I2C_status;
+  } I2C_status;
 
-void mw_init(void);
-I2C_status mw_write(uint8_t *data, uint8_t len);
-void mw_stop(); 
+  typedef enum : uint8_t {
+    NO_ERR = 0,
+    DEV_ERR = 1,
+    COM_ERR = 2,
+  } error;
+
+  I2C_status status;
+  void init();
+  error write(uint8_t *data, uint8_t len);
+  void stop();
+};
